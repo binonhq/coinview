86dfd3597bff2a0f2460b601a471a53c7b13f051
Nguyễn Hoàng Quân <quannguyen5@beeketing.net>
2025-03-29 18:56:21 +0700

    feat: final

--- api/src/server.js
+++ api/src/server.js
@@ -7,7 +7,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+}));
+  
 app.use(express.json());
 
 // Routes
